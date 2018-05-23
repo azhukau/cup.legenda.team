ef2d4ad79f43356fca5ed04ba33b255ebec20b88
Aliaksei Zhukau <zhukau.aliaksei@gmail.com>
2018-05-23 11:24:50 +0300

    node js

---
+++ bin/entry.js
@@ -0,0 +1,65 @@
+var xssFilters = require('xss-filters');
+const fs = require('fs');
+const http = require('http');
+var url = require('url');
+
+let jsdom = require('jsdom').JSDOM,
+
+uri = '../index.html',
+ 
+// the options that I will be giving to jsdom
+options = {
+    runScripts: 'dangerously',
+    resources: "usable"
+};
+
+const serv = http.createServer(function (req, res) {
+
+    var parsedurl = url.parse(req.url);
+
+    console.log(parsedurl);
+
+    var first = xssFilters.inHTMLData(q.first);
+    var last = xssFilters.inHTMLData(q.last);
+    var year = xssFilters.inHTMLData(q.year);
+    var team = xssFilters.inHTMLData(q.team);
+    var si = xssFilters.inHTMLData(q.si);
+    var course = xssFilters.inHTMLData(q.course);
+
+    jsdom.fromFile(uri, options).then(function (dom) {
+ 
+        let window = dom.window,
+        document = window.document;
+    
+        var entries = document.getElementById('entries');
+    
+        var row = entries.insertRow(1);
+    
+        row.innerHTML = `
+            <td>${last}</td>
+            <td>${first}</td>
+            <td>${year}</td>
+            <td>${team}</td>
+            <td>${si}</td>
+            <td>${course}</td>
+            <td>&nbsp;</td>
+    
+        `;
+        
+        var o = dom.serialize();
+    
+        fs.writeFile(uri,o,function(err) {
+            if (err) return console.error(err);
+        });    
+     
+    }).catch (function (e) {
+     
+        console.log(e);
+     
+    });    
+
+    }
+
+);
+
+serv.listen(1390);
